e4cf89d40d5d02de772b70984026eff5087a3b63
mattperkins <github@mattperkins.com>
2018-05-17 15:20:34 +0100

    add entry point option

--- Props.js
+++ Props.js
@@ -1,12 +1,16 @@
+import Expo from 'expo' // add if this component is app entry point
+
 import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
 
 
-export default class Props extends React.Component{
+class Props extends React.Component{
     render() {
         let msg = `Hello ${this.props.msg}`
         return (
-            <Text>{msg}</Text>
+            <Text style={{marginTop: 200, textAlign: 'center'}}>{msg}</Text>
         )
     }
 }
+
+export default Expo.registerRootComponent(Props) // add to whichever component you wish as the entry point to your app
