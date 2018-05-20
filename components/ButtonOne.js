f597b82c5482f7907b3c9067b0d8988d6a138239
mattperkins <github@mattperkins.com>
2018-05-20 16:46:45 +0100

    add second button and handler

--- components/ButtonOne.js
+++ components/ButtonOne.js
@@ -5,15 +5,29 @@ import { View, Button, Alert } from 'react-native'
 
 
 export default class ButtonOne extends React.Component{
+    BtnTwoPress = ()=> Alert.alert("Button Two pressed!")
     render() {
         
         return (
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                
+                <View
+                    style={{backgroundColor: 'green', borderRadius: 6, padding: 6}}>
                 <Button 
-                    onPress={()=> Alert.alert("Button pressed!")}
-                    title="This is a button"    
+                    onPress={()=> Alert.alert("Button One pressed!")}
+                    title="ButtonOne"    
+                    color="white"
+                />
+                </View>
+
+                <View
+                    style={{marginTop: 50, backgroundColor: 'skyblue', borderRadius: 6, padding: 6}}>
+                <Button 
+                    onPress={this.BtnTwoPress}
+                    title="ButtonTwo"    
                     color="#333"
                 />
+                </View>
             </View>
         )
     }
