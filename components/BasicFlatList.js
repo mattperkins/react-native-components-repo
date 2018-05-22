7ece17263f45f058080e5ebc95f75b651f565c90
mattperkins <github@mattperkins.com>
2018-05-22 12:32:21 +0100

    add swipeout component

--- components/BasicFlatList.js
+++ components/BasicFlatList.js
@@ -1,11 +1,59 @@
 import React from 'react'
-import { StyleSheet, Image, Text, View, FlatList, Dimensions } from 'react-native'
+import { StyleSheet, Image, Text, View, FlatList, Dimensions, Alert } from 'react-native'
 import flatListData from '../darta/flatListData'
+import Swipeout from 'react-native-swipeout'
 
 class FlatListItem extends React.Component {
+  constructor(props) {
+  super(props)
+    this.state = {
+      activeRowKey: null
+    }
+  }
+
   render() {
+
+    const swipeSettings = {
+      autoClose: true,
+      onClose: (secId, rowId, direction) => {
+        if(this.state.activeRowKey != null) {
+          this.setState({
+              activeRowKey: null
+            })
+        }
+      },
+      onOpen: (secId, rowId, direction) => {
+        this.setState({
+              activeRowKey: this.props.item.key
+            })
+      },
+      right: [
+        {
+          onPress: () => {
+            Alert.alert(
+              'Alert',
+              'Are you sure you want to delete ?',
+              [
+                {text: 'No', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                {text: 'Yes', onPress: () => {
+                  flatListData.splice(this.props.index, 1)
+                }}
+              ],
+              { cancelable: true }
+            )      
+          },
+          text: 'Delete', type: 'delete'
+        } 
+      ],
+      rowId: this.props.index,
+      sectionId: 1
+    }
+
     return (
-      <View>
+      
+      <Swipeout {...swipeSettings}>
+
+        <View>
 
       <View
         style={{
@@ -28,6 +76,9 @@ class FlatListItem extends React.Component {
 
       <View style={{height: 1, backgroundColor: 'white'}}></View>
       </View>
+
+      </Swipeout>
+      
     )
   }
 }
